32a58a1f2e5f759ed256231d950b62995673bd58
Milad salamian <miladsalami1385@gmail.com>
2024-07-08 09:34:06 -0700

    add pagination component

--- components/templates/p-admin/users/UsersList.tsx
+++ components/templates/p-admin/users/UsersList.tsx
@@ -1,7 +1,7 @@
+import Pagination from "@/components/modules/pagination/Pagination";
 import Table from "@/components/modules/table/Table";
 import React from "react";
 import { FaEye, FaPencil, FaTrash } from "react-icons/fa6";
-import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
 
 function UsersList() {
   return (
@@ -111,21 +111,7 @@ function UsersList() {
           </Table.Row>
         </Table.Body>
       </Table>
-      <div className="flex items-center justify-between text-white rounded-b-md bg-[#363636] px-10 py-4 border-t border-zinc-400">
-        <p className="font-Dana">
-          <span className="font-bold">1</span> تا <span className="font-bold">2</span> - از <span className="font-bold">20</span> تا
-        </p>
-        <div className="flex items-center gap-x-2">
-          <button className="flex-center font-IranMedium w-[80px] hover:bg-namava transition-all gap-x-2 py-1 rounded-md flex items-center">
-            <HiChevronRight />
-            بعدی
-          </button>
-          <button className="flex-center font-IranMedium w-[80px] hover:bg-namava transition-all gap-x-2 py-1 rounded-md flex items-center">
-            قبلی
-            <HiChevronLeft />
-          </button>
-        </div>
-      </div>
+      <Pagination />
     </div>
   );
 }
