1ee229714b232ed8b64b23d778182fdbd99f0ce3
Milad salamian <miladsalami1385@gmail.com>
2024-07-17 12:45:27 -0700

    add ticketsList to p-admin

--- src/app/p-admin/tickets/page.tsx
+++ src/app/p-admin/tickets/page.tsx
@@ -1,10 +1,12 @@
 import Title from "@/components/modules/p-admin/Title";
+import TicketsList from "@/components/templates/p-admin/tickets/TicketsList";
 import React from "react";
 
 function Ticketspage() {
   return (
     <div>
-      <Title name="تیکت ها" />
+      <Title name="لیست تیکت ها" />
+      <TicketsList/>
     </div>
   );
 }
