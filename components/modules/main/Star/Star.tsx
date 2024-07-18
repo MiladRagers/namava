07c2df5f2dbb8a20d334d5ed831e38b154c6004f
Milad salamian <miladsalami1385@gmail.com>
2024-07-18 05:31:01 -0700

    refactor star styles

--- components/modules/main/Star/Star.tsx
+++ components/modules/main/Star/Star.tsx
@@ -9,13 +9,13 @@ interface Star {
 }
 function Star({ src, title, link }: Star) {
   return (
-    <Link href={link} className="block rounded-full w-[90px] h-[90px] sm:w-[140px] sm:h-[140px] md:w-[190px] md:h-[190px]" title={title}>
+    <Link href={link} className="block rounded-full w-[100px] h-[100px] sm:w-[140px] sm:h-[140px] md:w-[190px] md:h-[190px]" title={title}>
       <Image
         src={src}
         width={400}
         height={500}
         alt={title}
-        className="rounded-full w-[90px] h-[90px] sm:w-[140px] sm:h-[140px] md:w-[190px] md:h-[190px]"
+        className="rounded-full w-[100px] h-[100px] sm:w-[140px] sm:h-[140px] md:w-[190px] md:h-[190px]"
       />
       <h3 className="text-center mt-3 text-xs md:text-sm">{title}</h3>
     </Link>
