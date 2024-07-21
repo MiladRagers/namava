d408a00afa9ec2bf164f22e06bb47360b1030dfb
Milad salamian <miladsalami1385@gmail.com>
2024-07-21 02:59:54 -0700

    add main slider to blog page

---
+++ components/templates/article/MainSlider.tsx
@@ -0,0 +1,73 @@
+"use client";
+import Image from "next/image";
+import React from "react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+function MainSlider() {
+  return (
+    <div className="max-w-[1200px] mx-auto  my-24">
+      <Swiper pagination={{ clickable: true }} modules={[Pagination]}>
+        <SwiperSlide>
+          <Image
+            src={"/images/article/shogan.jpg"}
+            alt="shogan.jpg"
+                       className="rounded-t-md"
+            width={1920}
+            height={1080}
+          />
+          <div className="px-4 py-3 space-y-2 bg-namavaBlack rounded-b-md">
+            <h2 className="text-white font-IranMedium">
+              «شوگان»؛ نگاه تازه‌ به یک داستان ماندگار
+            </h2>
+            <div className="text-[#ebebeb] text-[13px] flex items-center gap-x-4">
+              <div>
+                <span>نویسنده : </span>
+                <span>علی افتخاری</span>
+              </div>
+              <span className="w-px h-3.5 bg-[#aaa]"></span>
+              <div>28 اسفند 1402</div>
+              <span className="w-px h-3.5 bg-[#aaa]"></span>
+              <div>
+                <span>زمان مطالعه : </span>
+                <span>10 دقیقه</span>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            src={"/images/article/article2.jpg"}
+            alt="shogan.jpg"
+                       className="rounded-t-md"
+            width={1920}
+            height={1080}
+
+          />
+          <div className="px-4 py-3 space-y-2 bg-namavaBlack rounded-b-md">
+            <h2 className="text-white font-IranMedium">
+              «شوگان»؛ نگاه تازه‌ به یک داستان ماندگار
+            </h2>
+            <div className="text-[#ebebeb] text-[13px] flex items-center gap-x-4">
+              <div>
+                <span>نویسنده : </span>
+                <span>علی افتخاری</span>
+              </div>
+              <span className="w-px h-3.5 bg-[#aaa]"></span>
+              <div>28 اسفند 1402</div>
+              <span className="w-px h-3.5 bg-[#aaa]"></span>
+              <div>
+                <span>زمان مطالعه : </span>
+                <span>10 دقیقه</span>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+}
+
+export default MainSlider;
---
+++ src/app/(main)/blog/page.tsx
@@ -0,0 +1,12 @@
+import MainSlider from "@/components/templates/article/MainSlider";
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      <MainSlider />
+    </div>
+  );
+}
+
+export default page;
