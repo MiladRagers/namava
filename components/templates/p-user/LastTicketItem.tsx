83e3cf42a6533c97462b4295229537be84cc1618
Milad salamian <miladsalami1385@gmail.com>
2024-07-26 05:30:01 -0700

    add stat box to tickets page in p-user

--- components/templates/p-user/LastTicketItem.tsx
+++ components/templates/p-user/LastTicketItem.tsx
@@ -1,8 +1,9 @@
+import Link from "next/link";
 import React from "react";
 
 function LastTicketItem() {
   return (
-    <div className="text-white bg-[#121212] p-4 rounded-md flex items-center justify-between">
+    <Link href="/p-user/tickets/1" className="text-white bg-[#121212] p-4 rounded-md flex items-center justify-between">
       <div className="space-y-3">
         <h4>سرمایه گذاری</h4>
         <div className="bg-white text-[#121212] flex-center py-2 rounded-md">
@@ -13,7 +14,7 @@ function LastTicketItem() {
         <h5 className="font-Dana">1403/05/02</h5>
         <p>پاسخ داده شده</p>
       </div>
-    </div>
+    </Link>
   );
 }
 
--- src/app/p-user/tickets/page.tsx
+++ src/app/p-user/tickets/page.tsx
@@ -1,6 +1,11 @@
 import Filter from "@/components/modules/Filter/Filter";
+import StatBox from "@/components/modules/p-admin/StatBox";
 import TicketsList from "@/components/templates/p-user/TicketsList";
 import React from "react";
+import { FaClosedCaptioning } from "react-icons/fa6";
+import { HiOutlineBriefcase } from "react-icons/hi2";
+import { LiaComments } from "react-icons/lia";
+import { MdAccessTime } from "react-icons/md";
 
 function page() {
   return (
@@ -14,6 +19,36 @@ function page() {
           { label: "بسته شده", slug: "closed" },
         ]}
       />
+      <div className="grid  grid-cols-2 md:grid-cols-3 lg:grid-cols-4 text-white gap-3 md:gap-5 mt-5">
+        <StatBox
+          icon={<LiaComments className="text-2xl md:text-3xl lg:text-4xl" />}
+          title="تعداد کامنت ها"
+          color="bg-[#075985]"
+          value={29232}
+        />
+        <StatBox
+          icon={
+            <HiOutlineBriefcase className="text-2xl md:text-3xl lg:text-4xl" />
+          }
+          title="پاسخ داده شده"
+          color="bg-amber-600"
+          value={29232}
+        />
+        <StatBox
+          icon={<MdAccessTime className="text-2xl md:text-3xl lg:text-4xl" />}
+          title="در انتظار پاسخ"
+          color="bg-[#3730a3]"
+          value={29232}
+        />
+        <StatBox
+          icon={
+            <FaClosedCaptioning className="text-2xl md:text-3xl lg:text-4xl" />
+          }
+          title="بسته شده"
+          color="bg-red-600"
+          value={29232}
+        />
+      </div>
       <TicketsList />
     </div>
   );
