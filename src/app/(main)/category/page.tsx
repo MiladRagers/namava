de29afe279ac79548a7f59a2b31cb47b1b5029ed
Milad salamian <miladsalami1385@gmail.com>
2024-07-27 14:03:25 -0700

    add category single page

--- src/app/(main)/category/page.tsx
+++ src/app/(main)/category/page.tsx
@@ -5,26 +5,26 @@ function Categories() {
   return (
     <div className="text-white container pt-20 pb-10">
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3.5">
-        <CategoryCard image="iranian.jpg" title="ایرانی" link="" />
-        <CategoryCard image="animation.jpg" title="انیمه و انیمیشن" link="" />
-        <CategoryCard image="namava.jpg" title="دوبله نماوا" link="" />
-        <CategoryCard image="pardis.jpg" title="پردیس نماوا" link="" />
-        <CategoryCard image="korean.jpg" title="کره ای" link="" />
-        <CategoryCard image="indian.jpg" title="هندی" link="" />
-        <CategoryCard image="turkish.jpg" title="ترکی" link="" />
-        <CategoryCard image="best.jpg" title="برترین ها" link="" />
-        <CategoryCard image="oskar.jpg" title="اسکار" link="" />
-        <CategoryCard image="treaning.jpg" title="آموزش و سرگرمی" link="" />
-        <CategoryCard image="action.jpg" title="اکشن" link="" />
-        <CategoryCard image="comedy.jpg" title="کمدی" link="" />
-        <CategoryCard image="scare.jpg" title="ترسناک" link="" />
-        <CategoryCard image="lovely.jpg" title="عاشقانه" link="" />
-        <CategoryCard image="drum.jpg" title="درام" link="" />
-        <CategoryCard image="science.jpg" title="علمی تخیلی" link="" />
-        <CategoryCard image="crime.jpg" title="جنایی" link="" />
-        <CategoryCard image="kid.jpg" title="کودک" link="" />
-        <CategoryCard image="storley.jpg" title="ماجراجویی" link="" />
-        <CategoryCard image="classic.jpg" title="کلاسیک" link="" />
+        <CategoryCard image="iranian.jpg" title="ایرانی" link="/category/1" />
+        <CategoryCard image="animation.jpg" title="انیمه و انیمیشن" link="/category/1" />
+        <CategoryCard image="namava.jpg" title="دوبله نماوا" link="/category/1" />
+        <CategoryCard image="pardis.jpg" title="پردیس نماوا" link="/category/1" />
+        <CategoryCard image="korean.jpg" title="کره ای" link="/category/1" />
+        <CategoryCard image="indian.jpg" title="هندی" link="/category/1" />
+        <CategoryCard image="turkish.jpg" title="ترکی" link="/category/1" />
+        <CategoryCard image="best.jpg" title="برترین ها" link="/category/1" />
+        <CategoryCard image="oskar.jpg" title="اسکار" link="/category/1" />
+        <CategoryCard image="treaning.jpg" title="آموزش و سرگرمی" link="/category/1" />
+        <CategoryCard image="action.jpg" title="اکشن" link="/category/1" />
+        <CategoryCard image="comedy.jpg" title="کمدی" link="/category/1" />
+        <CategoryCard image="scare.jpg" title="ترسناک" link="/category/1" />
+        <CategoryCard image="lovely.jpg" title="عاشقانه" link="/category/1" />
+        <CategoryCard image="drum.jpg" title="درام" link="/category/1" />
+        <CategoryCard image="science.jpg" title="علمی تخیلی" link="/category/1" />
+        <CategoryCard image="crime.jpg" title="جنایی" link="/category/1" />
+        <CategoryCard image="kid.jpg" title="کودک" link="/category/1" />
+        <CategoryCard image="storley.jpg" title="ماجراجویی" link="/category/1" />
+        <CategoryCard image="classic.jpg" title="کلاسیک" link="/category/1" />
       </div>
     </div>
   );
