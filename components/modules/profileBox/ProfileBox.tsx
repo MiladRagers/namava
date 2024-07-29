bc1715859ae8a03acc1f5778166fd6ce92a93494
Milad salamian <miladsalami1385@gmail.com>
2024-07-29 14:55:48 -0700

    refcator profile box

--- components/modules/profileBox/ProfileBox.tsx
+++ components/modules/profileBox/ProfileBox.tsx
@@ -1,10 +1,9 @@
 import Image from "next/image";
-import Link from "next/link";
 import React from "react";
 
 function ProfileBox({ profile }: any) {
   return (
-    <Link href={`/${profile._id}`}>
+    <div>
       <Image
         src={profile.image}
         width={1920}
@@ -13,7 +12,7 @@ function ProfileBox({ profile }: any) {
         className="rounded-full w-[100px] h-[100px] md:w-[150px] md:h-[150px]"
       />
       <h2 className="text-center mt-3">{profile.name}</h2>
-    </Link>
+    </div>
   );
 }
 
