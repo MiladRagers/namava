dd760ad21f2fdf3b4f9bf5a3f0fb6458dceb9ff5
Milad salamian <miladsalami1385@gmail.com>
2024-08-02 12:07:46 -0700

    refactor delete user function

--- src/libs/actions/user.ts
+++ src/libs/actions/user.ts
@@ -3,11 +3,21 @@ import UserModel from "@/src/models/user";
 import ProfileModel from "@/src/models/profile";
 import connectToDB from "@/src/configs/db";
 import { revalidatePath } from "next/cache";
+import { isValidObjectId } from "mongoose";
 
 export const deleteUser = async (userId: string) => {
   try {
     connectToDB();
+
+    if (!isValidObjectId) {
+      return {
+        message: "ایدی مد نظر معتبر نمی باشد",
+        status: 422,
+      };
+    }
+
     const user = await UserModel.findOne({ _id: userId });
+    
     if (!user) {
       return {
         message: "کاربری با این ایدی یافت نشد",
@@ -23,6 +33,17 @@ export const deleteUser = async (userId: string) => {
       message: "کاربر با موفقیت حذف شد",
       status: 200,
     };
+  } catch (error) {
+    return {
+      message: "اتصال اینترنت خود را بررسی کنید",
+      status: 500,
+    };
+  }
+};
+
+export const createNewUser = async () => {
+  try {
+    connectToDB();
   } catch (error) {
     return {
       message: "اتصال اینترنت خود را بررسی کنید",
