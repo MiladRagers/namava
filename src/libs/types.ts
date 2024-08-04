fc6834c886c614c8d6772b42a595dffdc4a387f0
Milad salamian <miladsalami1385@gmail.com>
2024-08-04 07:44:05 -0700

    get specific data of actor in single biography page

--- src/libs/types.ts
+++ src/libs/types.ts
@@ -7,4 +7,8 @@ export type TComment = {
   onShow: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
+export type TParams = {
+  params: { [key: string]: string | string[] | undefined };
+};
+
 export const baseURL = "http://localhost:3000";
