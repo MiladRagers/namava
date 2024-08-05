128ae8ac5638ba2561b270ed93c50d8547dfd1fe
Milad salamian <miladsalami1385@gmail.com>
2024-08-05 12:27:49 -0700

    add contact us & department and subdepartment model

---
+++ src/models/contactus.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const schema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const model = mongoose.models.Contact || mongoose.model("Contact", schema);
+
+export default model;
