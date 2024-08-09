14630ca2be3b1d90868afaf7cbe19d0f2c80ae03
Milad salamian <miladsalami1385@gmail.com>
2024-08-09 06:12:50 -0700

    add search input in movie list

--- src/app/p-admin/movies/page.tsx
+++ src/app/p-admin/movies/page.tsx
@@ -1,3 +1,4 @@
+import Search from "@/components/modules/p-admin/Search";
 import Title from "@/components/modules/p-admin/Title";
 import AddNewFilm from "@/components/templates/p-admin/film/AddNewFilm";
 import FilmList from "@/components/templates/p-admin/film/FilmList";
@@ -23,7 +24,10 @@ async function MoviesPage({ searchParams }: TAdminPage) {
         subCategories={JSON.parse(JSON.stringify(allSubCategories))}
         stars={JSON.parse(JSON.stringify(allStarts))}
       />
-      <Title name="لیست فیلم و سریال ها" />
+      <div className="flex items-start md:items-center justify-between flex-col md:flex-row gap-y-3">
+        <Title name="لیست فیلم و سریال ها" />
+        <Search />
+      </div>
       <FilmList
         movies={JSON.parse(JSON.stringify(movies.allMovies))}
         counts={movies.counts}
