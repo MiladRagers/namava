f77d2aac9aa2d6c53b9efc160cfd26c7f64b5f8d
Milad salamian <miladsalami1385@gmail.com>
2024-08-18 12:43:13 -0700

    fix search bug  in p-admin

--- components/modules/p-admin/Search.tsx
+++ components/modules/p-admin/Search.tsx
@@ -1,10 +1,11 @@
 "use client";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { FaMagnifyingGlass, FaXmark } from "react-icons/fa6";
 import { useDebouncedCallback } from "use-debounce";
 
 function Search() {
+  const [search, setSearch] = useState("");
   const searchParams = useSearchParams();
   const query = searchParams.get("q") || "";
   const router = useRouter();
@@ -22,17 +23,22 @@ function Search() {
   const deleteSearchParam = () => {
     params.delete("q");
     router.replace(`${pathname}?${params}`);
+    setSearch("");
   };
   return (
     <div className="bg-namavaBlack w-full md:w-[20%] rounded-md px-3 py-2 flex items-center justify-between">
       <div className="flex items-center gap-x-2">
         <FaMagnifyingGlass className="text-white" />
         <input
+          value={search}
           className="border-none outline-none w-full bg-transparent text-white"
           type="text"
           defaultValue={query}
           placeholder="جستجو کنید"
-          onChange={(e) => handleSearch(e.target.value)}
+          onChange={(e) => {
+            handleSearch(e.target.value);
+            setSearch(e.target.value);
+          }}
         />
       </div>
       {query && (
