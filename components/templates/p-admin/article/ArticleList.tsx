f8425756f155bbea5ea61b88c0d218174f197078
Milad salamian <miladsalami1385@gmail.com>
2024-08-18 12:34:34 -0700

    generate articles in article page

--- components/templates/p-admin/article/ArticleList.tsx
+++ components/templates/p-admin/article/ArticleList.tsx
@@ -1,10 +1,18 @@
+"use client";
+import EmptyBox from "@/components/modules/p-admin/EmptyBox";
 import Pagination from "@/components/modules/pagination/Pagination";
 import Table from "@/components/modules/table/Table";
 import Image from "next/image";
-import React from "react";
+import React, { useOptimistic } from "react";
 import { FaCheck, FaPencil, FaTrash, FaXmark } from "react-icons/fa6";
 
-function ArticleList() {
+function ArticleList({ articles, counts }: any) {
+  const [optimisticArticles, deleteOptimistc] = useOptimistic(
+    articles,
+    (allArticles, id) => {
+      return allArticles.filter((article: any) => article._id !== id);
+    }
+  );
   return (
     <div className="users-list mt-10 overflow-hidden bg-namavaBlack  rounded-md">
       <Table>
@@ -19,59 +27,40 @@ function ArticleList() {
           <th>عملیات</th>
         </Table.Header>
         <Table.Body>
-          <Table.Row>
-            <td>1</td>
-            <td className="!p-0 md:!p-5">
-              <Image
-                src="/images/article/shogan.jpg"
-                className="w-32 object-cover h-10 md:h-14 rounded-md mx-auto"
-                alt=""
-                width={1920}
-                height={1080}
-              />
-            </td>
-            <td>«شوگان»؛ نگاه تازه‌ </td>
-            <td>non-fiction</td>
-            <td>میلاد سلامیان</td>
-            <td>منتشر شده</td>
-            <td>1403/04/15</td>
-            <td>
-              <div className="flex items-center justify-center gap-x-3 md:gap-x-6 child:cursor-pointer">
-                <FaTrash className="text-red-600 text-base md:text-lg" />
-                <FaPencil className="text-sky-600 text-base md:text-lg" />
-                <FaCheck className="text-green-500 text-base md:text-lg" />
-                <FaXmark className="text-amber-500 text-base md:text-lg" />
-              </div>
-            </td>
-          </Table.Row>
-          <Table.Row>
-            <td>1</td>
-            <td className="!p-0 md:!p-5">
-              <Image
-                src="/images/article/article2.jpg"
-                className="w-32 object-cover h-10 md:h-14 rounded-md mx-auto"
-                alt=""
-                width={1920}
-                height={1080}
-              />
-            </td>
-            <td>«شوگان»؛ نگاه تازه‌ </td>
-            <td>non-fiction</td>
-            <td>میلاد سلامیان</td>
-            <td>منتشر شده</td>
-            <td>1403/04/15</td>
-            <td>
-              <div className="flex items-center justify-center gap-x-3 md:gap-x-6 child:cursor-pointer">
-                <FaTrash className="text-red-600 text-base md:text-lg" />
-                <FaPencil className="text-sky-600 text-base md:text-lg" />
-                <FaCheck className="text-green-500 text-base md:text-lg" />
-                <FaXmark className="text-amber-500 text-base md:text-lg" />
-              </div>
-            </td>
-          </Table.Row>
+          {articles.map((article: any, index: number) => (
+            <Table.Row>
+              <td>{index + 1}</td>
+              <td className="!p-0 md:!p-5">
+                <Image
+                  src={article.image}
+                  className="w-32 object-cover h-10 md:h-14 rounded-md mx-auto"
+                  alt={article.title}
+                  width={1920}
+                  height={1080}
+                />
+              </td>
+              <td>{article.title.slice(0, 26) + " ... "}</td>
+              <td>{article.link}</td>
+              <td>{article.creator.name}</td>
+              <td>{article.isAccept ? "منتشر شده" : "پیش نویس"}</td>
+              <td>{new Date(article.createdAt).toLocaleDateString("fa-IR")}</td>
+              <td>
+                <div className="flex items-center justify-center gap-x-3 md:gap-x-6 child:cursor-pointer">
+                  <FaTrash className="text-red-600 text-base md:text-lg" />
+                  <FaPencil className="text-sky-600 text-base md:text-lg" />
+                  <FaCheck className="text-green-500 text-base md:text-lg" />
+                  <FaXmark className="text-amber-500 text-base md:text-lg" />
+                </div>
+              </td>
+            </Table.Row>
+          ))}
         </Table.Body>
       </Table>
-      <Pagination />
+      {optimisticArticles.length > 0 ? (
+        <Pagination count={counts} />
+      ) : (
+        <EmptyBox title="اطلاعات مورد نظر یافت نشد" />
+      )}
     </div>
   );
 }
--- src/app/p-admin/articles/page.tsx
+++ src/app/p-admin/articles/page.tsx
@@ -1,17 +1,27 @@
 import Title from "@/components/modules/p-admin/Title";
 import AddNewArticle from "@/components/templates/p-admin/article/AddNewArticle";
 import ArticleList from "@/components/templates/p-admin/article/ArticleList";
-import { getAllMoviesWithOutPagination } from "@/src/libs/service/services";
+import {
+  getAllArticles,
+  getAllMoviesWithOutPagination,
+} from "@/src/libs/service/services";
+import { TAdminPage } from "@/src/libs/types";
 import React from "react";
 
-async function ArticlesPage() {
-  const movies = await getAllMoviesWithOutPagination();
+async function ArticlesPage({ searchParams }: TAdminPage) {
+  const [movies, data]: any = await Promise.all([
+    getAllMoviesWithOutPagination(),
+    getAllArticles(+searchParams.page, searchParams.q),
+  ]);
   return (
     <>
       <Title name="ایجاد مقاله جدید" />
       <AddNewArticle movies={JSON.parse(JSON.stringify(movies))} />
       <Title name="لیست مقاله ها" />
-      <ArticleList />
+      <ArticleList
+        articles={JSON.parse(JSON.stringify(data.articles))}
+        counts={data.counts}
+      />
     </>
   );
 }
