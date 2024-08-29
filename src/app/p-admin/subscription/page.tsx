cc111abf7fd34eaea6096b13bf1dc7ad83235548
Milad salamian <miladsalami1385@gmail.com>
2024-08-29 13:44:54 -0700

    add search input in subscriptions list

--- src/app/p-admin/subscription/page.tsx
+++ src/app/p-admin/subscription/page.tsx
@@ -1,3 +1,4 @@
+import Search from "@/components/modules/p-admin/Search";
 import Title from "@/components/modules/p-admin/Title";
 import AddNewSubscription from "@/components/templates/p-admin/Subscription/AddNewSubscription";
 import SubscriptionList from "@/components/templates/p-admin/Subscription/SubscriptionList";
@@ -7,9 +8,12 @@ function Subscription() {
   return (
     <div>
       <Title name="ساخت اشتراک" />
-      <AddNewSubscription/>
-      <Title name="لیست اشتراک ها" />
-      <SubscriptionList/>
+      <AddNewSubscription />
+      <div className="flex items-start md:items-center justify-between flex-col md:flex-row gap-y-3">
+        <Title name="لیست اشتراک ها" />
+        <Search />
+      </div>
+      <SubscriptionList />
     </div>
   );
 }
