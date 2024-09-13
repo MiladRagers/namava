21b21c266854204a610d7e46e92ca2426dbccbc4
Milad salamian <miladsalami1385@gmail.com>
2024-09-13 12:48:56 -0700

    get specific user info in p-admin

--- src/app/p-user/account/page.tsx
+++ src/app/p-user/account/page.tsx
@@ -1,40 +1,54 @@
+import { checkUserSubscription } from "@/src/libs/service/services";
+import { formatDate } from "@/src/utils/funcs";
+import { authUser } from "@/src/utils/serverHelper";
 import Link from "next/link";
 import React from "react";
 
-function page() {
+async function page() {
+  const [user, subscription] : any = await Promise.all([
+    authUser(),
+    checkUserSubscription(),
+  ]);
+
   return (
     <>
       <div className="bg-namavaBlack rounded-md p-6 text-white">
         <div className="flex items-center justify-between">
           <h1 className="text-xl">اطلاعات شخصی</h1>
-          <Link href={"account/information"} className="text-namava">ویرایش اطلاعات</Link>
+          <Link href={"account/information"} className="text-namava">
+            ویرایش اطلاعات
+          </Link>
         </div>
         <div className="mt-8">
           <ul className="space-y-5 text-base">
             <li>
               <span className="text-[#d4d4d4]">نام : </span>
-              <span>میلاد </span>
+              <span>{user.name.split(" ")[0]}</span>
             </li>
             <li>
               <span className="text-[#d4d4d4]">نام خانوادگی : </span>
-              <span>میلاد سلامیان </span>
+              <span>{user.name.split(" ")[1]}</span>
             </li>
 
             <li>
               <span className="text-[#d4d4d4]">نام کاربری : </span>
-              <span>milad1385</span>
+              <span>{user.username}</span>
             </li>
             <li>
-              <span className="text-[#d4d4d4]">استان : </span>
-              <span>کرج </span>
+              <span className="text-[#d4d4d4]">اشتراک : </span>
+              <span>
+                {subscription.hasSubscription
+                  ? `${subscription.remainingDays} روز`
+                  : "فعال نیست"}
+              </span>
             </li>
             <li>
               <span className="text-[#d4d4d4]">تاریخ تولد : </span>
               <span>1385/03/19 </span>
             </li>
             <li>
-              <span className="text-[#d4d4d4]">جنیست : </span>
-              <span>مرد</span>
+              <span className="text-[#d4d4d4]">استان : </span>
+              <span>کرج</span>
             </li>
           </ul>
         </div>
@@ -42,32 +56,30 @@ function page() {
       <div className="bg-namavaBlack rounded-md p-6 text-white mt-5 ">
         <div className="flex items-center justify-between">
           <h1 className="text-xl">اطلاعات کاربری</h1>
-          <Link href={"account/information"} className="text-namava">ویرایش اطلاعات کاربری</Link>
+          <Link href={"account/information"} className="text-namava">
+            ویرایش اطلاعات کاربری
+          </Link>
         </div>
         <div className="mt-8">
           <ul className="space-y-5 text-base">
             <li>
               <span className="text-[#d4d4d4]">شماره تلفن : </span>
-              <span>09125237895 </span>
+              <span className="font-Dana">{user.phone}</span>
             </li>
             <li>
               <div>
-                <span className="text-[#d4d4d4]">ایمیل</span>
-                <span>milad@gmail.com</span>
+                <span className="text-[#d4d4d4]">ایمیل : </span>
+                <span>{user.email}</span>
               </div>
             </li>
 
             <li>
-              <span className="text-[#d4d4d4]">رمز عبور : </span>
-              <span>********</span>
-            </li>
-            <li>
               <span className="text-[#d4d4d4]">تعداد پروفایل : </span>
-              <span>5 تا </span>
+              <span>{user.profiles.length} تا </span>
             </li>
             <li>
               <span className="text-[#d4d4d4]">تاریخ عضویت : </span>
-              <span>1396/05/7 </span>
+              <span>{formatDate(user.createdAt)}</span>
             </li>
             <li>
               <span className="text-[#d4d4d4]">ژانر مورد علاقه : </span>
