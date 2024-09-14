1c96130a53cd5f58c8f0fd697cd0441943e80aa2
Milad salamian <miladsalami1385@gmail.com>
2024-09-14 10:33:41 -0700

    get and show all profiles in settings page

--- src/app/p-user/settings/page.tsx
+++ src/app/p-user/settings/page.tsx
@@ -1,71 +1,32 @@
+import { authUser } from "@/src/utils/serverHelper";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-function page() {
+async function page() {
+  const user = await authUser();
   return (
     <div className="bg-namavaBlack p-8 w-full  rounded-md md:w-1/2 mx-auto text-white">
       <ul className="w-full text-sm space-y-6">
-        <li className="flex items-center justify-between">
-          <div className="flex items-center gap-x-3">
-            <Image
-              src={"/images/user.png"}
-              alt="user.png"
-              width={1920}
-              height={1080}
-              className="w-[60px] h-[60px] rounded-full"
-            />
-            <span>
-              بزرگسال <span className="text-[#aaa]">(پیش فرض)</span>
-            </span>
-          </div>
-          <Link href={"/profile-list-edit/1"} className="text-namava">ویرایش پروفایل</Link>
-        </li>
-        <li className="flex items-center justify-between">
-          <div className="flex items-center gap-x-3">
-            <Image
-              src={"/images/user.png"}
-              alt="user.png"
-              width={1920}
-              height={1080}
-              className="w-[60px] h-[60px] rounded-full"
-            />
-            <span>
-              بزرگسال <span className="text-[#aaa]">(پیش فرض)</span>
-            </span>
-          </div>
-          <Link href={"/profile-list-edit/1"} className="text-namava">ویرایش پروفایل</Link>
-        </li>
-        <li className="flex items-center justify-between">
-          <div className="flex items-center gap-x-3">
-            <Image
-              src={"/images/user.png"}
-              alt="user.png"
-              width={1920}
-              height={1080}
-              className="w-[60px] h-[60px] rounded-full"
-            />
-            <span>
-              بزرگسال <span className="text-[#aaa]">(پیش فرض)</span>
-            </span>
-          </div>
-          <Link href={"/profile-list-edit/1"} className="text-namava">ویرایش پروفایل</Link>
-        </li>
-        <li className="flex items-center justify-between">
-          <div className="flex items-center gap-x-3">
-            <Image
-              src={"/images/user.png"}
-              alt="user.png"
-              width={1920}
-              height={1080}
-              className="w-[60px] h-[60px] rounded-full"
-            />
-            <span>
-              بزرگسال <span className="text-[#aaa]">(پیش فرض)</span>
-            </span>
-          </div>
-          <Link href={"/profile-list-edit/1"} className="text-namava">ویرایش پروفایل</Link>
-        </li>
+        {user.profiles.map((profile: any , index : number) => (
+          <li className="flex items-center justify-between">
+            <div className="flex items-center gap-x-3">
+              <Image
+                src={profile.image}
+                alt={profile.name}
+                width={1920}
+                height={1080}
+                className="w-[60px] h-[60px] rounded-full"
+              />
+              <span>
+                {profile.name} <span className="text-[#aaa]">{index === 0 ? "(پیش فرض)" :""}</span>
+              </span>
+            </div>
+            <Link href={`/profile-list-edit/${profile._id}`} className="text-namava">
+              ویرایش پروفایل
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
