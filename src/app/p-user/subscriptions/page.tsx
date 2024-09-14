304d6db82da41369e95bdeda5bb56fb181ee9963
Milad salamian <miladsalami1385@gmail.com>
2024-09-14 09:43:48 -0700

    get and show user subscription in user panel

--- src/app/p-user/subscriptions/page.tsx
+++ src/app/p-user/subscriptions/page.tsx
@@ -1,44 +1,65 @@
 import Button from "@/src/components/modules/auth/Button/Button";
+import { checkUserSubscription } from "@/src/libs/service/services";
+import { formatDate } from "@/src/utils/funcs";
+import { authUser } from "@/src/utils/serverHelper";
 import Link from "next/link";
 import React from "react";
+import { FaShop } from "react-icons/fa6";
 
-function page() {
+async function page() {
+  const [user, subscription]: any = await Promise.all([
+    authUser(),
+    checkUserSubscription(),
+  ]);
   return (
     <div className="bg-namavaBlack rounded-md p-6 text-white">
       <div className="flex items-center justify-between">
-        <h1 className="text-xl text-green-600">اطلاعات اشتراک فعال</h1>
+        <h1
+          className={`text-xl ${
+            subscription.hasSubscription ? "text-green-600" : "text-red-600"
+          }`}
+        >
+          {subscription.hasSubscription
+            ? "اطلاعات اشتراک فعال"
+            : "اشتراک غیر فعال"}
+        </h1>
       </div>
       <div className="mt-8">
-        <ul className="space-y-5 text-base">
-          <li>
-            <span className="text-[#d4d4d4]">تاریخ فعال سازی : </span>
-            <span>1403/03/19 </span>
-          </li>
-          <li>
-            <span className="text-[#d4d4d4]">تاریخ اتمام : </span>
-            <span>1403/06/1</span>
-          </li>
+        {subscription.hasSubscription ? (
+          <ul className="space-y-5 text-base">
+            <li>
+              <span className="text-[#d4d4d4]">تاریخ فعال سازی : </span>
+              <span>{formatDate(user.subscriptionStart)}</span>
+            </li>
+            <li>
+              <span className="text-[#d4d4d4]">تاریخ اتمام : </span>
+              <span>{formatDate(user.subscriptionEnd)}</span>
+            </li>
 
-          <li>
-            <span className="text-[#d4d4d4]">تعداد روز های باقی مانده : </span>
-            <span>90 روز</span>
-          </li>
-          <li>
-            <span className="text-[#d4d4d4]">خریدار : </span>
-            <span>میلاد سلامیان</span>
-          </li>
-          <li>
-            <span className="text-[#d4d4d4]">ایمیل : </span>
-            <span>milad@gmail.com</span>
-          </li>
-        </ul>
-
-        <div className="flex-center flex-col gap-y-12 mt-16">
-          <p>شما در حال حاضر اشتراک فعالی ندارید.</p>
-          <Link href={"/plans"}>
-            <Button className="!w-[200px]">خرید اشتراک</Button>
-          </Link>
-        </div>
+            <li>
+              <span className="text-[#d4d4d4]">
+                تعداد روز های باقی مانده :{" "}
+              </span>
+              <span>{subscription.remainingDays.toLocaleString("fa-IR")} روز</span>
+            </li>
+            <li>
+              <span className="text-[#d4d4d4]">خریدار : </span>
+              <span>{user.name}</span>
+            </li>
+            <li>
+              <span className="text-[#d4d4d4]">ایمیل : </span>
+              <span>{user.email}</span>
+            </li>
+          </ul>
+        ) : (
+          <div className="flex-center flex-col gap-y-12 mt-16">
+            <FaShop className="text-[50px] md:text-[100px]"/>
+            <p>شما در حال حاضر اشتراک فعالی ندارید.</p>
+            <Link href={"/plans"}>
+              <Button className="!w-[200px]">خرید اشتراک</Button>
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
