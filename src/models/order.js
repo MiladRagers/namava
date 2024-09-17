56155171d4d6152d15372710b41d95c960ccb027
Milad salamian <miladsalami1385@gmail.com>
2024-09-17 03:45:38 -0700

    add time stapms in order model

--- src/models/order.js
+++ src/models/order.js
@@ -1,26 +1,31 @@
 import mongoose from "mongoose";
 
-const schema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true,
+const schema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    orderNumber: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pay", "pending"],
+    },
   },
-  orderNumber: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  totalPrice: {
-    type: Number,
-    required: true,
-  },
-  status: {
-    type: String,
-    enum: ["pay", "pending"],
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const model = mongoose.models.Order || mongoose.model("Order", schema);
 
-export default model
+export default model;
